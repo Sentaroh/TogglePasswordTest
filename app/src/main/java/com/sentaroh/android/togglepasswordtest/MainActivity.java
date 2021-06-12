75d89d2d103fae3c9d342c5f7aa94c21e7002fde
Fumio Hoshino <gm.developer.fhoshino@gmail.com>
2021-06-12 10:52:00 +0900

    Initail commit

---
+++ app/src/main/java/com/sentaroh/android/togglepasswordtest/MainActivity.java
@@ -0,0 +1,116 @@
+package com.sentaroh.android.togglepasswordtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView message=(TextView)findViewById(R.id.message);
+
+        final TextInputLayout password_view=(TextInputLayout)findViewById(R.id.password_view);
+        final TextInputEditText password=(TextInputEditText)findViewById(R.id.password);
+        final TextInputLayout confirm_view=(TextInputLayout)findViewById(R.id.confirm_view);
+        final TextInputEditText confirm=(TextInputEditText)findViewById(R.id.confirm);
+
+        password_view.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (password.getTransformationMethod()!=null) {
+                    password.setTransformationMethod(null);
+                    confirm_view.setVisibility(TextInputLayout.GONE);
+                    password.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+                        @Override
+                        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                            return true;
+                        }
+                        @Override
+                        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                            menu.removeItem(android.R.id.cut);
+                            menu.removeItem(android.R.id.copy);
+                            menu.removeItem(android.R.id.shareText);
+                            return true;
+                        }
+                        @Override
+                        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                            return false;
+                        }
+                        @Override
+                        public void onDestroyActionMode(ActionMode mode) {
+                        }
+                    });
+                } else {
+                    password.setTransformationMethod(new PasswordTransformationMethod());
+                    password.setCustomSelectionActionModeCallback(null);
+                    confirm_view.setVisibility(TextInputLayout.VISIBLE);
+                    message.setText("");
+                }
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (password.getTransformationMethod()!=null) {
+                    if (!password.getText().toString().equals(confirm.getText().toString())) {
+                        message.setText("Password and confirm password unmatched");
+                    } else {
+                        message.setText("");
+                    }
+                }
+            }
+        });
+
+        confirm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (password.getTransformationMethod()!=null) {
+                    if (!password.getText().toString().equals(confirm.getText().toString())) {
+                        message.setText("Password and confirm password unmatched");
+                    } else {
+                        message.setText("");
+                    }
+                }
+            }
+        });
+
+
+
+    }
+}
